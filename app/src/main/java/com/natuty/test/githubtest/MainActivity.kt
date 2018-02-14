f25c3bc41313fb4f80c55f08cb92b4a42882f7a7
natuty <2604961784@qq.com>
2018-02-14 20:29:30 +0800

    helloWorld

--- app/src/main/java/com/natuty/test/githubtest/MainActivity.kt
+++ app/src/main/java/com/natuty/test/githubtest/MainActivity.kt
@@ -2,11 +2,14 @@ package com.natuty.test.githubtest
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setText("helloword");
     }
 }
